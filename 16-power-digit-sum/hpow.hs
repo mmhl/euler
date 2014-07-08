bb29efbd2d35d24beb4b99a036ee5391d2673991
Michal Mieczkowski <m.mieczkowsk@samsung.com>
2014-07-08 15:54:53 +0200

    Better solution for Haskell calculating digits of number in task 16-power-digit-sum

--- 16-power-digit-sum/hpow.hs
+++ 16-power-digit-sum/hpow.hs
@@ -1,9 +1,4 @@
-toDigits :: (Num a, Integral a) => a -> [a]
-toDigits 0 = []
-toDigits x = toDigits ( x `div` 10) ++ [x `mod` 10]
-
-euler18 = sum $ toDigits $ 2^1000
-
+euler18 = sum . map(\x -> read [x] :: Int) $ show $ 2^1000
 main = do (putStrLn.show) euler18
 
 
