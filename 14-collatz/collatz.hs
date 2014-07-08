d7b448404c1f8dbc0fd318a5c7b139d3ed942550
Michal Mieczkowski <m.mieczkowsk@samsung.com>
2014-07-08 14:20:40 +0200

    Added Haskell solution for problem 14-collatz

--- 14-collatz/collatz.hs
+++ 14-collatz/collatz.hs
@@ -1,2 +1,8 @@
-collatz :: Int -> [Int]
+collatz :: Integer -> [Integer]
+collatz 1 = [1]
+collatz x = if odd x then x : collatz next_odd
+	else x : collatz next_even
+	where
+	next_odd = 3*x + 1
+	next_even = x `div` 2
 
