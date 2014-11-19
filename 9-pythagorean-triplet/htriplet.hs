f3e128454607191e6da0a11465008a618f0c5bc3
Michal Mieczkowski <mhl@time-machine.fake>
2014-11-19 11:10:47 +0100

    Import only <$> from Control.Applicative

--- 9-pythagorean-triplet/htriplet.hs
+++ 9-pythagorean-triplet/htriplet.hs
@@ -1,5 +1,5 @@
 import Data.List
-import Control.Applicative
+import Control.Applicative ((<$>))
 
 main = do
         let (Just triplet) = find (\xs-> sum xs == 1000) $(\(m,n) -> sort [m^2-n^2, 2*m*n, m^2+n^2]) <$> [(m,n) | m <- [2,3..], n <- [1..pred m], odd(m-n)]
