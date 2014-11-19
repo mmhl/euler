a813b873c36b8034f4050a53a3dea48529d4a103
Michal Mieczkowski <mhl@time-machine.fake>
2014-11-19 11:04:28 +0100

    Added more concise haskell solution to problem 9-pythagorean-triplet

---
+++ 9-pythagorean-triplet/htriplet.hs
@@ -0,0 +1,6 @@
+import Data.List
+import Control.Applicative
+
+main = do
+        let (Just triplet) = find (\xs-> sum xs == 1000) $(\(m,n) -> sort [m^2-n^2, 2*m*n, m^2+n^2]) <$> [(m,n) | m <- [2,3..], n <- [1..pred m], odd(m-n)]
+        putStrLn $ show $ product triplet
