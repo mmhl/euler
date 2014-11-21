d7b7c70ad5011299ecc5c19d34442f41342813e8
Michal Mieczkowski <mhl@time-machine.fake>
2014-11-21 15:03:30 +0100

    Microoptimized version of problem 14-collatz

--- 14-collatz/collatz.cpp
+++ 14-collatz/collatz.cpp
@@ -4,41 +4,51 @@
 #include <algorithm>
 #include <unordered_map>
 
-using namespace std;
-static unordered_map<int, int> steps;
+#ifdef __GNUC__
+#define likely(x)       __builtin_expect(!!(x), 1)
+#define unlikely(x)     __builtin_expect(!!(x), 0)
+#else
+#define likely(x)       (x)
+#define unlikely(x)     (x)
+#endif
 
-int collatz(unsigned int val) {
-        if(val == 1)
+using namespace std;
+static unordered_map<int, int> map_steps;
+
+inline int collatz(unsigned int val) {
+
+        if((val==1)) 
                 return 1;
 
-        auto got = steps.find(val);
-        if(got != steps.end()) {
+        auto got = map_steps.find(val);
+        if(got != map_steps.end()) {
                 return got->second;
         }
 
-        if(val%2 == 0) {
-                int val_steps = 1+collatz(val/2);
-                steps[val] = val_steps;
-                return steps[val];
-        }
-        else {
-                int val_steps = 1+collatz(3*val+1);
-                steps[val] = val_steps;
-                return steps[val];
-        }
+        int val_steps = 1;
+        if((val & 1) == 0) 
+                val_steps += collatz(val >> 1);
+        else 
+                val_steps += collatz(3*val+1);
+
+        map_steps[val] = val_steps;
+        return val_steps;
 }
 
 int main() 
 {
-        steps[1] = 0;
-        int max = 0;
-        for(int i = 2; i < 10000000; i++) {
+        map_steps.reserve(6000000);
+        int key, max;
+        key = max = 0;
+        for(int i = 3; i < 1000000; i += 2) {
                 int steps = collatz(i);
-                if(steps > max) {
-                        cout << "New score: " << i << " with " << steps << endl;
+                if(unlikely(steps > max)) {
+                        //cout << "New score: " << i << " with " << steps << "\n";
                         max = steps;
+                        key = i;
                 }
         }
+        cout << key << " " << max << endl; 
         return 0;
 }
 
